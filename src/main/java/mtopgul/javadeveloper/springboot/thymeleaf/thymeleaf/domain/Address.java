0babba36492b3515e57778984af9bf349a31a914
muhammed.topgul <muhammedtopgul.29@gmail.com>
2020-05-13 23:57:23 +0300

    Configured Lombok. Closes #9

--- src/main/java/mtopgul/javadeveloper/springboot/thymeleaf/thymeleaf/domain/Address.java
+++ src/main/java/mtopgul/javadeveloper/springboot/thymeleaf/thymeleaf/domain/Address.java
@@ -1,37 +1,23 @@
 package mtopgul.javadeveloper.springboot.thymeleaf.thymeleaf.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 /**
  * Created by Muhammed Topgul.
  * Date: 13.05.2020
  * Time: 21:49
  */
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Address {
 
     private String street;
     private String zipCode;
 
-    public Address() {
-    }
-
-    public Address(String street, String zipCode) {
-        this.street = street;
-        this.zipCode = zipCode;
-    }
-
-    public String getStreet() {
-        return street;
-    }
-
-    public void setStreet(String street) {
-        this.street = street;
-    }
-
-    public String getZipCode() {
-        return zipCode;
-    }
-
-    public void setZipCode(String zipCode) {
-        this.zipCode = zipCode;
-    }
 }
--- src/main/java/mtopgul/javadeveloper/springboot/thymeleaf/thymeleaf/domain/Person.java
+++ src/main/java/mtopgul/javadeveloper/springboot/thymeleaf/thymeleaf/domain/Person.java
@@ -1,52 +1,30 @@
 package mtopgul.javadeveloper.springboot.thymeleaf.thymeleaf.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 /**
  * Created by Muhammed Topgul.
  * Date: 13.05.2020
  * Time: 21:49
  */
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Person {
 
     private Integer id;
     private String name;
     private Address address;
 
-    public Person() {
-    }
-
     public Person(Integer id, String name) {
         this.id = id;
         this.name = name;
     }
 
-    public Person(Integer id, String name, Address address) {
-        this.id = id;
-        this.name = name;
-        this.address = address;
-    }
 
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Address getAddress() {
-        return address;
-    }
-
-    public void setAddress(Address address) {
-        this.address = address;
-    }
 }
